d5b667f18ecec4b18fbf709554777d21c639cc92
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2023-01-11 08:27:02 -0800

    Taak 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == __"main"__
+if __name__ == __"main"__:
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a+b))
+    print("{} + {} = {}".format(a, b, add(a+b)))
